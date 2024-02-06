315b7c9b27579ff50fea49d5d939d5eaf7ce2c2b
Roed <Geofery10@hotmail.com>
2024-02-06 12:43:51 +0100

    refractor python solution to be a list comprehension

--- 01a_File_formats_bonanza/python_solution/dynamicFileReader.py
+++ 01a_File_formats_bonanza/python_solution/dynamicFileReader.py
@@ -1,9 +1,14 @@
-import random
+files = [
+    "01a_File_formats_bonanza/me.csv",
+    "01a_File_formats_bonanza/me.json",
+    "01a_File_formats_bonanza/me.txt",
+    "01a_File_formats_bonanza/me.xml",
+    "01a_File_formats_bonanza/me.yaml"
+]
 
-files = ["01a_File_formats_bonanza/me.csv", "01a_File_formats_bonanza/me.json", "01a_File_formats_bonanza/me.txt", "01a_File_formats_bonanza/me.xml","01a_File_formats_bonanza/me.yaml" ]
+file_contents = [
+    f"Content of file with extension {file_path.split('.')[1]}:\n{open(file_path, 'r').read()}\n"
+    for file_path in files
+]
 
-f = open(random.choice(files), "r")
-print(f.read())
-
-f.close()
-
+print(''.join(file_contents))
