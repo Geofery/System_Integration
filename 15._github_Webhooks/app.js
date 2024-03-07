835859310e2be18c32dd4f04ae14191a0e32cbc3
Roed <Geofery10@hotmail.com>
2024-03-07 10:51:48 +0100

    webhooks running with github repo/webhooks

--- 15._github_Webhooks/app.js
+++ 15._github_Webhooks/app.js
@@ -1,10 +1,20 @@
 import express from "express";
-
+//ssh -R roed:80:127.0.0.1:8080 serveo.net 
+//nodemon app.js
+//https://github.com/Geofery/webhooks/settings/hooks
+//Lav webhook med den rigtige url, url hedder: https://roed.serveo.net/githubwebhookform
+//Hvis ssh -R er kørt og den har lavet en server. 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.post("/githubwebhookjson", (req, res) => {
+    console.log(req.body);
+    res.sendStatus(204);
+});
+
+app.post("/githubwebhookform", (req, res) => {
     console.log(req.body);
     res.sendStatus(204);
 });
